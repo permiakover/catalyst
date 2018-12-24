8d6a5c46beb25bbac8323e56fa0ae28ab411b2dd
permiakovsadev <permiakovsadev@gmail.com>
2018-12-24 23:02:37 +0300

    ДЗ №2 выполнено

--- task_6.py
+++
@@ -1,6 +0,0 @@
-# 6. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
-ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
-
-POS=int(input('pos = '))
-
-print('Буква с номером ', POS, ' в алфавите это - ', ALPHABET[POS-1])
